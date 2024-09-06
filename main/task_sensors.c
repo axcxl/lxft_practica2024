5fc7471aa0f7756eb535bf020c04a6ce4cbf17f4
Andrei Cioran <Andrei.Cioran@belden.com>
2024-09-06 14:16:12 +0000

    Fix issue with sending task
    It was sending data too fast because xLastWakeTime was not properly init.

--- main/task_sensors.c
+++ main/task_sensors.c
@@ -6,17 +6,17 @@ const static char *TAG = "sensors";
 
 void task_sensors(void* msg_queue)
 { 
-    TickType_t xLastWakeTime;
+    TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xTimeIncrement = pdMS_TO_TICKS(1000); // run every 1 second
     int sent_num  = 0;
-       const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+
     while(1){
         vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);
         if (xQueueGenericSend(*(QueueHandle_t*)msg_queue, (void *)&sent_num, portMAX_DELAY, queueSEND_TO_BACK) != pdTRUE) 
         {
-            ESP_LOGI(TAG, "Queue full\n");
+            ESP_LOGE(TAG, "Queue full");
         }
-        vTaskDelay(xDelay);
+
         sent_num++;
-        }
+    }
 }
